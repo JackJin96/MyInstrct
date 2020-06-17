4edf6b70906c54defd2725b7118c2711acd5df6d
JackJin96 <hanchong1996@hotmail.com>
2020-06-17 00:29:54 -0400

    added header button and changed grid colors

--- navigation/InstructNavigator.js
+++ navigation/InstructNavigator.js
@@ -1,10 +1,14 @@
+import React from "react";
+
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import InstructionListScreen from "../screens/InstructionListScreen";
 import InstructionDetailScreen from "../screens/InstuctionDetailScreen";
+import PlusHeaderButton from "../components/CustomHeaderButton";
 import Colors from "../constants/Colors";
 
 const InstructNavigator = createStackNavigator(
@@ -14,6 +18,26 @@ const InstructNavigator = createStackNavigator(
       navigationOptions: {
         title: "Home",
         headerTitleStyle: { alignSelf: "center" },
+        headerRight: () => (
+          <HeaderButtons HeaderButtonComponent={PlusHeaderButton}>
+            <Item
+              title="Add Task"
+              iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
+              onPress={() => {
+                // navData.navigation.navigate("NewTask");
+                alert("PLus button!");
+              }}
+            />
+            <Item
+              title="Delete Task"
+              iconName={Platform.OS === "android" ? "md-trash" : "ios-trash"}
+              onPress={() => {
+                // navData.navigation.navigate("NewTask");
+                alert("Delete button!");
+              }}
+            />
+          </HeaderButtons>
+        ),
       },
     },
     InstructionList: InstructionListScreen,
@@ -25,6 +49,7 @@ const InstructNavigator = createStackNavigator(
         backgroundColor:
           Platform.OS === "android" ? Colors.primaryColor : "white",
       },
+      headerTitleAlign: 'center',
       headerTintColor:
         Platform.OS === "android" ? "white" : Colors.primaryColor,
     },
