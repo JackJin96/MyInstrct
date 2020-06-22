6309132425cf49660877a5e3b64d20f17af744cf
JackJin96 <hanchong1996@hotmail.com>
2020-06-22 14:30:42 -0400

    added tab navigator

--- navigation/InstructNavigator.js
+++ navigation/InstructNavigator.js
@@ -3,12 +3,16 @@ import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createDrawerNavigator } from "react-navigation-drawer";
+import { Ionicons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import InstructionListScreen from "../screens/InstructionListScreen";
 import InstructionDetailScreen from "../screens/InstuctionDetailScreen";
 import AddCategoryScreen from "../screens/AddCategoryScreen";
+import FavoriteScreen from "../screens/FavoriteScreen";
 import SignInScreen from "../screens/SignInScreen";
 
 import Colors from "../constants/Colors";
@@ -18,7 +22,7 @@ const MainStackNavigator = createStackNavigator(
     Categories: {
       screen: CategoriesScreen,
       navigationOptions: {
-        title: "Home",
+        title: "MyInstruct",
       },
     },
     InstructionList: InstructionListScreen,
@@ -72,9 +76,49 @@ const SignInNavigator = createStackNavigator(
   }
 );
 
+const tabScreenConfig = {
+  Instructions: {
+    screen: MainStackNavigator,
+    navigationOptions: {
+      tabBarLabel: "Instructions",
+      tabBarIcon: (tabInfo) => {
+        return (
+          <Ionicons name="ios-paper" size={25} color={tabInfo.tintColor} />
+        );
+      },
+      tabBarColor: Colors.primaryColor,
+    },
+  },
+  Favorites: {
+    screen: FavoriteScreen,
+    navigationOptions: {
+      tabBarLabel: "Favorites",
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+      },
+      tabBarColor: Colors.accentColor,
+    },
+  },
+};
+
+const FavTabNavigator =
+  Platform.OS === "android"
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeColor: "white",
+        shifting: true,
+        barStyle: {
+          backgroundColor: Colors.primaryColor,
+        },
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: {
+          activeTintColor: Colors.primaryColor,
+        },
+      });
+
 const MainNavigator = createDrawerNavigator(
   {
-    Home: MainStackNavigator,
+    Home: FavTabNavigator,
     SignIn: {
       screen: SignInNavigator,
       navigationOptions: {
---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { View, Text, StyleSheet } from "react-native";
+
+const FavoriteScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>Favorite Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FavoriteScreen;
